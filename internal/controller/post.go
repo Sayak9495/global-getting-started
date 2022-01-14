ba31ee9818711f636366cb9d27f96d316ba103fb
shashank.prakash <shashank.prakash@exotel.in>
2022-01-14 22:29:23 +0530

    adding images

--- internal/controller/post.go
+++ internal/controller/post.go
@@ -20,12 +20,11 @@ func Getting_started(ctx *gin.Context) {
 
 	question_object := strikeObject.Question("").
 		QuestionText().
-		SetTextToQuestion("Hi "+name+"! Welcome to strike developers community", "Text Description, getting used for testing purpose.")
+		SetTextToQuestion("Hi "+name+"! fghWelcome to strike developers community", "Text Description, getting used for testing purpose.")
 
 	answer_object := question_object.Answer(true).AnswerCardArray(strike.VERTICAL_ORIENTATION)
-	answer_object.AnswerCard().SetHeaderToAnswer(1, strike.HALF_WIDTH).
-		AddGraphicRowToAnswer(strike.PICTURE_ROW, []string{"media/welcome.jpg"}, []string{}).
-		AddTextRowToAnswer(strike.H4, "Cool stuff about what we really are", "#41a800", true)
+	answer_object.AnswerCard().SetHeaderToAnswer(1, strike.HALF_WIDTH).AddGraphicRowToAnswer(strike.PICTURE_ROW, []string{"https://logodix.com/logo/2205276.png"}, []string{})
+	//answer_object.AnswerCard().SetHeaderToAnswer(1, strike.HALF_WIDTH).AddTextRowToAnswer(strike.H4, "Cool stuff about what we really are", "#41a800", true)
 
 	ctx.JSON(200, strikeObject)
 }
